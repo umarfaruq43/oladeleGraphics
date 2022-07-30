1ee95301b06cb7cf04e1fbe0d62740a1bbd451a5
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-07-30 01:37:21 +0100

    update: work  page head added

--- pages/work.jsx
+++ pages/work.jsx
@@ -2,9 +2,12 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import Works from "../components/portfollio/Works";
 import Layout from "../Layout/Layout";
+import Pixel from "../pixel";
 const portfolio = () => {
   return (
     <Layout>
+      <Pixel name="FacebookPixel" />
+
       <Works />
     </Layout>
   );
