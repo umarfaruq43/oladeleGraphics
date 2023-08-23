8c1e3e25301b066256bd4c96f6f9674f1d295067
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2023-08-23 18:22:19 +0100

    slider update

--- components/landingpage/TestimoniesSlider.jsx
+++ components/landingpage/TestimoniesSlider.jsx
@@ -22,21 +22,21 @@ const TestimoniesSlider = () => {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 1,
+                    slidesToScroll: 2,
                 },
             },
             {
                 breakpoint: 820,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1,
+                    slidesToScroll: 2,
                 },
             },
             {
                 breakpoint: 560,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1, // Change this to 1
+                    slidesToScroll: 2, // Change this to 1
                 },
             },
         ],
